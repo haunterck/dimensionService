3130a3c6ed217f92e991f9e6cd9cdc0905f95bf5
Bet Mendoza <sbm2391@hotmail.com>
2018-09-08 17:05:33 -0500

    fix error

--- healthy_finance/models/__init__.py
+++ healthy_finance/models/__init__.py
@@ -2,4 +2,4 @@ from .att_plan import AttPlan
 from .cliente import Cliente
 from .lu_casfim import LuCasfim
 from .lu_ieps import LuIeps
-from -bancomerpay import Bancomerpay
+from .bancomerpay import Bancomerpay
